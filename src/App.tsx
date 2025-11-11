cced1a570c7d697fefb191054c5ef3e7bca80953
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-11-11 13:31:52 -0500

    removed unneeded files, navbar and app files set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
