0abd9a25a4808f0cfeddf0f0f1c39634e5175e33
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-12-16 16:06:29 -0500

    img added to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import Sponsors from '../components/Sponsors';
+// @ts-ignore
+import DickensCover from '../assets/DickensCover.jpg';
+import '../styles/home.css';
 
 export default function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
+      <section className='cover'>
+        <img src={DickensCover} alt="Dickens Christmas in Skaneateles" style={{ maxWidth: '100%', height: 'auto' }} />
+        there should be an image here
+      </section>
       <Sponsors />
     </div>
   );
